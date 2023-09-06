0ddb0d0bfa4148b9e65b0abc854e9bf3dc88c129
barnovdw <barnovdw@gmail.com>
2023-09-06 12:58:53 +0200

    Fixed errors on controllers

--- CannyStore.UI/Controllers/CategoryController.cs
+++ CannyStore.UI/Controllers/CategoryController.cs
@@ -10,84 +10,89 @@ namespace CannyStore.UI.Controllers
 {
     public class CategoryController : Controller
     {
-        IRepository<ProductCategory> _repository;
+        IRepository<ProductCategory> context;
         public CategoryController(IRepository<ProductCategory> categoryContext)
         {
-            _repository = categoryContext;
+            context = categoryContext;
         }
-
         // GET: Category
         public ActionResult Index()
         {
-            List<ProductCategory> categories = _repository.Collection().ToList();
-            return View();
+            List<ProductCategory> categories = context.Collection().ToList();
+            return View(categories);
         }
-        public ActionResult Create() 
+        public ActionResult Create()
         {
-            ProductCategory category = new ProductCategory();
-            return View(category);
+            ProductCategory productCategory = new ProductCategory();
+            return View(productCategory);
         }
         [HttpPost]
-        public ActionResult Create(ProductCategory category)
+        public ActionResult Create(ProductCategory productCategory)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                return View(category);
+                return View(productCategory);
             }
             else
             {
-                _repository.Insert(category);
-                _repository.Commit();
+                context.Insert(productCategory);
+                context.Commit();
                 return RedirectToAction("Index");
             }
         }
-        public ActionResult Edit(string Id) 
+
+        public ActionResult Edit(string Id)
         {
-            ProductCategory category = _repository.Find(Id);
-            if (category == null)
-            {
-                return HttpNotFound();
-            }
-            else
-                return View(category);
-        }
-        [HttpPost]
-        public ActionResult Edit(ProductCategory category,string Id)
-        {
-            ProductCategory c = _repository.Find(Id);
-            if (c == null)
+            ProductCategory productCategory = context.Find(Id);
+            if (productCategory == null)
             {
                 return HttpNotFound();
             }
             else
             {
-                c.Category = category.Category;
-                _repository.Commit();
+                return View(productCategory);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCategory, string Id)
+        {
+            ProductCategory CategoryToEdit = context.Find(Id);
+            if (CategoryToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                CategoryToEdit.Category = productCategory.Category;
+                context.Commit();
                 return RedirectToAction("Index");
             }
         }
+
         public ActionResult Delete(string Id)
         {
-            ProductCategory c = _repository.Find(Id);
-            if (c == null)
+            ProductCategory CategoryToDelete = context.Find(Id);
+            if (CategoryToDelete == null)
             {
                 return HttpNotFound();
             }
             else
             {
-                return View(c);
+                return View(CategoryToDelete);
             }
         }
         [HttpPost]
         [ActionName("Delete")]
         public ActionResult ConfirmDelete(string Id)
         {
-            ProductCategory c = _repository.Find(Id);
-            if (c == null)
-            { return HttpNotFound(); }
+            ProductCategory CategoryToDelete = context.Find(Id);
+            if (CategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
             else
             {
-                _repository.Delete(Id); 
+                context.Delete(Id);
                 return RedirectToAction("Index");
             }
         }
